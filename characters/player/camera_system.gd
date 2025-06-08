83ed9523526dbc74323e8d481d0d1d0ecaa3473d
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-06-07 19:13:02 -0500

    swap camera

--- characters/player/camera_system.gd
+++ characters/player/camera_system.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
 @export var character : CharacterBody3D
+@export var edge_spring_arm: SpringArm3D
+@export var transition_speed_camera_swap: float = 0.2
 
+@onready var default_spring_arm_edge_length := edge_spring_arm.spring_length
+
+var camera_tween: Tween
 var camera_rotation: Vector2 = Vector2.ZERO
 var camera_sensitivity: float = 0.002
 var max_y_rotation: int = 180
@@ -18,17 +23,28 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var mouse_event: Vector2 = event.relative
 		camera_look(mouse_event)
+	if event.is_action_pressed("swap_camera"):
+		swap_camera_alignment()
 		
 func camera_look(mouse_movement: Vector2) -> void:
 	camera_rotation += mouse_movement
 	reset_rotation()
 	character.transform.basis = Basis()
 	camera_rotation.y = clamp(camera_rotation.y, -max_y_rotation, max_y_rotation+180)
-	print(camera_rotation.y)
 	character.rotate_object_local(Vector3(0,1,0), -camera_rotation.x * camera_sensitivity)
 	rotate_object_local(Vector3(1,0,0), -camera_rotation.y * camera_sensitivity)
 
+func swap_camera_alignment() -> void:
+	#var new_pos: float = new_pos * -default_spring_arm_edge_length
+	default_spring_arm_edge_length = -default_spring_arm_edge_length
+	set_spring_arm_postition(default_spring_arm_edge_length, transition_speed_camera_swap)
 	
+func set_spring_arm_postition(pos: float, speed: float) -> void:
+	if camera_tween:
+		camera_tween.kill()
+	camera_tween = get_tree().create_tween()
+	print("este es el camera tween ", camera_tween)
+	camera_tween.tween_property(edge_spring_arm, "spring_length", pos, speed)
 	
 func reset_rotation() -> void:
 	var current_scale = transform.basis.get_scale()
