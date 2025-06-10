398828f41f16774b5f7d53e3c5fefc2b5912d018
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-06-09 19:50:27 -0500

    camera aim

--- characters/player/camera_system.gd
+++ characters/player/camera_system.gd
@@ -2,9 +2,17 @@ extends Node3D
 
 @export var character : CharacterBody3D
 @export var edge_spring_arm: SpringArm3D
+@export var rear_spring_arm: SpringArm3D
+@export var camera: Camera3D 
 @export var transition_speed_camera_swap: float = 0.2
+@export var aim_edge_spring_length: float = 0.5
+@export var aim_rear_spring_length: float = 0.5
+@export var aim_speed: float = 0.2
+@export var camera_fov_aim: float = 55
 
-@onready var default_spring_arm_edge_length := edge_spring_arm.spring_length
+@onready var default_spring_arm_edge_length :float = edge_spring_arm.spring_length
+@onready var default_spring_arm_rear_length :float = rear_spring_arm.spring_length
+@onready var default_camera_fov :float = camera.fov
 
 var camera_tween: Tween
 var camera_rotation: Vector2 = Vector2.ZERO
@@ -25,7 +33,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera_look(mouse_event)
 	if event.is_action_pressed("swap_camera"):
 		swap_camera_alignment()
-		
+	if event.is_action_pressed("aim"):
+		enter_aim()
+	if event.is_action_released("aim"):
+		exit_aim()
+	
 func camera_look(mouse_movement: Vector2) -> void:
 	camera_rotation += mouse_movement
 	reset_rotation()
@@ -36,13 +48,13 @@ func camera_look(mouse_movement: Vector2) -> void:
 
 func swap_camera_alignment() -> void:
 	default_spring_arm_edge_length = -default_spring_arm_edge_length
+	#var new_pos: float = default_spring_arm_edge_length * -sign(edge_spring_arm.spring_length)
 	set_spring_arm_postition(default_spring_arm_edge_length, transition_speed_camera_swap)
 	
 func set_spring_arm_postition(pos: float, speed: float) -> void:
 	if camera_tween:
 		camera_tween.kill()
 	camera_tween = get_tree().create_tween()
-	print("este es el camera tween ", camera_tween)
 	camera_tween.tween_property(edge_spring_arm, "spring_length", pos, speed)
 	
 func reset_rotation() -> void:
@@ -50,3 +62,37 @@ func reset_rotation() -> void:
 	var new_basis = Basis()
 	new_basis.scaled(current_scale)
 	transform.basis = new_basis
+
+func enter_aim():
+	if camera_tween:
+		camera_tween.kill()
+	camera_tween = get_tree().create_tween()
+	camera_tween.set_parallel(true)
+	camera_tween.tween_property(camera, "fov", camera_fov_aim, aim_speed)
+	camera_tween.tween_property(
+		edge_spring_arm, 
+		"spring_length", 
+		aim_edge_spring_length * sign(edge_spring_arm.spring_length), 
+		aim_speed)
+	camera_tween.tween_property(
+		rear_spring_arm , 
+		"spring_length", 
+		aim_rear_spring_length, 
+		aim_speed)
+
+func exit_aim():
+	if camera_tween:
+		camera_tween.kill()
+	camera_tween = get_tree().create_tween()
+	camera_tween.set_parallel(true)
+	camera_tween.tween_property(camera, "fov", default_camera_fov, aim_speed)
+	camera_tween.tween_property(
+		edge_spring_arm, 
+		"spring_length", 
+		default_spring_arm_edge_length  * sign(edge_spring_arm.spring_length), 
+		aim_speed)
+	camera_tween.tween_property(
+		rear_spring_arm , 
+		"spring_length", 
+		default_spring_arm_rear_length, 
+		aim_speed)
